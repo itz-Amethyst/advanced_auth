0cf0b1c6f0442ec0c1a1f6898ea0e7d7fa09cb99
itz-amethyst <pransermi@gmail.com>
2024-01-08 14:55:55 +0330

    feat: user view register

--- account/views.py
+++ account/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
 
-# Create your views here.
+from .models import User
+from .serializers import UserRegisterSerializer
+from rest_framework.response import Response
+
+
+
+class RegisterUserView(GenericAPIView):
+    serializer_class = UserRegisterSerializer
+
+    def post( self, request ):
+        user_data = request.data
+        serializer = self.serializer_class(data = user_data)
+        if serializer.is_valid(raise_exception = True):
+            serializer.save()
+            user: User = serializer.data
+            #! send email validation code
+            return Response({
+                "data": user,
+                "message": f"hi {user.username} thanks for choosing us , your verification code is"
+            }, status = status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
