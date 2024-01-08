b72aa76d9b8289e89589d8e118a5022377b93569
itz-amethyst <pransermi@gmail.com>
2024-01-08 14:32:48 +0330

    feat: user model

--- Advanced_Auth/settings.py
+++ Advanced_Auth/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
     }
 }
 
+#! Auth Model
+AUTH_USER_MODEL = "account.User"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- account/models.py
+++ account/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from .managers.user import UserManager
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length = 255, unique = True, verbose_name =_("Email Address"))
+    username = models.CharField(max_length = 50, unique = True, verbose_name =_("Username"))
+    is_staff = models.BooleanField(default = False)
+    is_superuser = models.BooleanField(default = False)
+    is_verified = models.BooleanField(default = False)
+    is_active = models.BooleanField(default = False)
+    date_joined = models.DateTimeField(auto_now_add = True)
+    last_login = models.DateTimeField(auto_now = True)
+
+    USERNAME_FIELD = "username"
+
+    REQUIRED_FIELDS = ["email"]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.email} / {self.username}"
+
+    def tokens( self ):
+        pass
