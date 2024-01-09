54e7403aeb2f3d64ac51c687eec2f1a718a9598a
itz-amethyst <pransermi@gmail.com>
2024-01-09 20:53:39 +0330

    feat: added set and verify password urls

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import RegisterUserView, VerifyUserEmail, LoginUserView, CheckIfLoggedIn
+from .views import RegisterUserView, VerifyUserEmail, LoginUserView, CheckIfLoggedIn, PasswordResetConfirmView, PasswordResetView, SetNewPassword
 
 urlpatterns = [
     path("register/", RegisterUserView.as_view(), name='register'),
     path("verify/", VerifyUserEmail.as_view(), name='verify'),
     path("login/", LoginUserView.as_view(), name='login'),
     path("profile/", CheckIfLoggedIn.as_view(), name='profile'),
+    path("password-reset/", PasswordResetView.as_view(), name='password-reset'),
+    path("password-reset-confirm/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    path("set-new-password/" , SetNewPassword.as_view() , name = 'set-new-password') ,
+
 ]
