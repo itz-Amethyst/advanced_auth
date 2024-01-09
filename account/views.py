8098243345f85c72b59473ee1f6c7075e3488525
itz-amethyst <pransermi@gmail.com>
2024-01-09 20:53:20 +0330

    feat: added set and verify password views

--- account/views.py
+++ account/views.py
@@ -1,15 +1,16 @@
-
-from django.shortcuts import render
+from django.shortcuts import redirect
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from .models import User, OneTimePassword
-from .serializers import UserRegisterSerializer , LoginUserSerializer , VerifyUserEmailSerializer , \
-    PasswordResetSerializer
+from .serializers import UserRegisterSerializer , LoginUserSerializer , VerifyUserEmailSerializer ,PasswordResetSerializer , SetNewPasswordSerializer
 from rest_framework.response import Response
 
 from .utils.OTP import send_code_to_user
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import smart_str, DjangoUnicodeDecodeError
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 
 class RegisterUserView(GenericAPIView):
@@ -81,3 +82,27 @@ class PasswordResetView(GenericAPIView):
         return Response({'message': 'a link has been send to your email'}, status = status.HTTP_200_OK)
 
 
+# Todo combine them together when user clicked the link on email redirects to set-new password ant filled the uid and token in header
+class PasswordResetConfirmView(GenericAPIView):
+
+    def get( self, request, uidb64, token ):
+        try:
+            user_id = smart_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(id = user_id)
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                return Response({'message': 'token is invalid or expired'}, status = status.HTTP_401_UNAUTHORIZED)
+
+            return Response({'message': 'success', 'uidb64': uidb64, 'token': token}, status = status.HTTP_200_OK)
+
+            # Needs rework on it
+            # redirect_url = f'/set-new-password/?uidb64={uidb64}&token={token}'
+            # return redirect(redirect_url)
+        except DjangoUnicodeDecodeError:
+            return Response({'message': 'token is invalid or expired'}, status = status.HTTP_401_UNAUTHORIZED)
+
+class SetNewPassword(GenericAPIView):
+    serializer_class = SetNewPasswordSerializer
+    def patch( self, request ):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid(raise_exception = True):
+            return Response({"message": "password reset was successful"}, status = status.HTTP_200_OK)
