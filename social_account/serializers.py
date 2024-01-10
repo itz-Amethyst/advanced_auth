2c0f9ce230d9f794ab40036ddb77aaba3d90ad57
itz-amethyst <pransermi@gmail.com>
2024-01-10 14:33:37 +0330

    feat: added google auth api

---
+++ social_account/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .dep.auth import Google, register_social_user
+from django.conf import settings
+from rest_framework.exceptions import AuthenticationFailed
+
+
+
+class GoogleAuthSerializer(serializers.Serializer):
+    access_token = serializers.CharField(min_length = 6)
+
+
+    @staticmethod
+    def validate_access_token( access_token ):
+        google_user_data = Google.validate(access_token)
+        try:
+            user_id = google_user_data['sub']
+        except:
+            raise serializers.ValidationError("this token is invalid or has been expired")
+
+        if google_user_data['aud'] != settings.GOOGLE_CLIENT_ID:
+            raise AuthenticationFailed(detail = "Could not verify the user")
+
+        email = google_user_data['email']
+        username = f"{google_user_data['given_name']}_{google_user_data['family_name']}"
+        provider = "google"
+
+        return register_social_user(provider, email, username)
--- social_account/views.py
+++ social_account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializers import GoogleAuthSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+
+class GoogleAuthView(GenericAPIView):
+    serializer_class = GoogleAuthSerializer
+
+    def post( self, request ):
+        serializer = self.serializer_class
+        if serializer.is_valid(raise_exception = True):
+            data = (serializer.validated_data['access_token'])
+            return Response(data, status = status.HTTP_200_OK)
+
+        return Response(serializer.errors, status = status.HTTP_406_NOT_ACCEPTABLE)
+
