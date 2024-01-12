7c5ee9ad1c01eff7cf4a54c41eb74af0e1c720e9
itz-amethyst <pransermi@gmail.com>
2024-01-12 20:43:55 +0330

    feat: register page

--- Frontend/src/pages/Register.svelte
+++ Frontend/src/pages/Register.svelte
@@ -1,21 +1,49 @@
 <script>
-    let email = "";
-    let password = "";
-  
-    const handleRegister = () => {
-      // Implement your registration logic here
-    };
-  </script>
-  
-  <div>
-    <h2>Register</h2>
-    <form on:submit={handleRegister}>
-      <label>Email:</label>
-      <input type="email" bind:value={email} required />
-  
-      <label>Password:</label>
-      <input type="password" bind:value={password} required />
-  
-      <button type="submit">Register</button>
+  let email = "";
+  let password = "";
+
+  const handleRegister = () => {
+    // Implement your registration logic here
+  };
+</script>
+
+<div class="form-container">
+  <div class="wrapper">
+    <h2>Create your account</h2>
+    <form action="post">
+      <div class="form-group">
+        <label for="">Email Address:</label>
+        <input type="text" class="email-form" name="email" />
+      </div>
+
+      <div class="form-group">
+        <label for="">Username:</label>
+        <input type="text" class="email-form" name="username" />
+      </div>
+
+      <div class="form-group">
+        <label for="">Password:</label>
+        <input type="text" class="email-form" name="password" />
+      </div>
+
+      <div class="form-group">
+        <label for="">Confirm Password:</label>
+        <input type="text" class="email-form" name="confirm_password" />
+      </div>
+
+      <input type="submit" value="Submit" class="submitButton" />
     </form>
+    <h3 class="text-option">Or</h3>
+
+    <div class="social-container">
+
+      <div class="githubContainer">
+        <button>Sign up with Github</button>
+      </div>
+      <div class="googleContainer">
+        <button>Sign up with Google</button>
+      </div>
+    </div>
+    
   </div>
+</div>
