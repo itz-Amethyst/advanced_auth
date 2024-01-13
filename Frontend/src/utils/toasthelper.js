b006c156b72bfd54e7716bfb5e161a9a2d95cd2b
itz-amethyst <pransermi@gmail.com>
2024-01-13 22:03:09 +0330

    feat: added inf toast message and axios pack

--- Frontend/src/utils/toasthelper.js
+++ Frontend/src/utils/toasthelper.js
@@ -12,3 +12,14 @@ export function showToast(title, body, theme) {
     onRemove: () => {},
   });
 }
+
+export function showToastInfDuration(title="Loading", body="Please wait until the proccess finish...", theme="info") {
+    toasts.add({
+      title,
+      description: body,
+      duration: 999000,
+      type: theme,
+      placement: "bottom-right",
+      onClick: () => {},
+    });
+  }
