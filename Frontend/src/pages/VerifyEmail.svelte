199f00323c9db8ffe45d5b85b20a092929897d6f
itz-amethyst <pransermi@gmail.com>
2024-01-14 14:25:29 +0330

    feat: verify page

--- Frontend/src/pages/VerifyEmail.svelte
+++ Frontend/src/pages/VerifyEmail.svelte
@@ -1,12 +1,53 @@
+<script>
+  import axios from "axios";
+  import {BASE_URL} from "../utils/constants"
+  import { navigate } from "svelte-routing";
+  import { showToast } from "../utils/toasthelper";
+  import { isValidInput } from "../utils/helper";
+
+  let otpCode = "";
+
+  const handleOtpSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!isValidInput(otpCode)) {
+      showToast("Error","Invalid input. Please enter a positive integer.","error");
+      return;
+    }
+
+    try {
+      console.log(BASE_URL);
+      const res = await axios.post(`${BASE_URL}/api/auth/verify/`, {
+        otp_code: otpCode,
+      });
+
+      const resp = res.data;
+
+      if (res.status === 200) {
+        navigate("/login");
+        showToast("Success", resp.message, "success");
+      }
+
+    } catch (error) {
+      showToast("Error", error.response.data.message, "error");
+    }
+  };
+</script>
+
 <div class="form-container">
   <div class="wrapper">
     <h2>Verify your account</h2>
-    <form action="post">
+    <form action="post" on:submit={handleOtpSubmit}>
       <div class="form-group">
         <label for="">Otp Code:</label>
-        <input type="text" class="email-form" name="otp" />
+        <input
+          type="text"
+          class="email-form"
+          name="otpCode"
+          bind:value={otpCode}
+        />
       </div>
-      <input type="submit" class="vbtn" value="Submit"/>
+      <input type="submit" class="vbtn" value="Submit" />
     </form>
   </div>
 </div>
--- Frontend/src/utils/helper.js
+++ Frontend/src/utils/helper.js
@@ -1,3 +1,9 @@
 export function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+// Function to check if the input is a positive integer
+export function isValidInput(input) {
+    const parsedInput = parseInt(input, 10);
+    return !isNaN(parsedInput) && parsedInput > 0 && Number.isInteger(parsedInput);
+  };
