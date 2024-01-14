844950e91c43b7a458c1d156f28fa73262621ed8
itz-amethyst <pransermi@gmail.com>
2024-01-14 21:19:48 +0330

    feat: improved axiosInstance

--- Frontend/src/auth/axiosInstance.js
+++ Frontend/src/auth/axiosInstance.js
@@ -1,14 +1,44 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
+import dayjs from "dayjs";
+import { jwtDecode } from "jwt-decode";
 
-const token = localStorage.getItem("access") ? JSON.parse(localStorage.getItem("access")) : ""
-const refresh_token = localStorage.getItem("refresh_token") ? JSON.parse(localStorage.getItem("refresh_token")) : ""
+
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
+
+let accessToken = localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : ""
+let refresh_token = localStorage.getItem("refresh_token") ? JSON.parse(localStorage.getItem("refresh_token")) : ""
 
 const axiosInstanse = axios.create({
     baseURL:BASE_URL,
+    cancelToken: source.token,
     headers: {
-        Authorization: localStorage.getItem('access') ? `Bearer ${token}` : null
+        Authorization: localStorage.getItem('token') ? `Bearer ${accessToken}` : ""
     }
 })
 
+axiosInstanse.interceptors.request.use(async req => {
+    if (accessToken) {
+        req.headers.Authorization = localStorage.getItem('token') ? `Bearer ${accessToken}` : ""
+        const user = jwtDecode(accessToken)
+        const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1
+        if (!isExpired) {
+            console.log("not expired");
+            return req
+        }
+        
+        console.log("isExpired:", isExpired)
+        const res = await axios.post(`${BASE_URL}/api/auth/token/refresh/`, {refresh: refresh_token})
+        console.log('new_accesstoken: ',res.data.access)
+        localStorage.setItem('token', JSON.stringify(res.data.access))
+        req.headers.Authorization = `Bearer ${res.data.access}`
+        return req
+    }
+
+    else {
+        req.headers.Authorization = localStorage.getItem('token') ? `Bearer ${JSON.parse(localStorage.getItem('token'))}` : " "
+        return req 
+    }
+});
 export default axiosInstanse
