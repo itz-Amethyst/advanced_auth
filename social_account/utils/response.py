5e908ad626d1943b7374f2f6e69fa7413e3b119d
itz-amethyst <pransermi@gmail.com>
2024-01-17 19:03:11 +0330

    fix: response helper

--- social_account/utils/response.py
+++ social_account/utils/response.py
@@ -5,18 +5,18 @@ from rest_framework.exceptions import AuthenticationFailed
 from account.models import User
 
 
-def send_response(email):
-    login_user = authenticate(email = email , password = settings.SOCIAL_AUTH_PASSWORD)
-    user_tokens = login_user.generate_token()
+def send_response(username, request):
+    user = authenticate(request , username = username , password = settings.SOCIAL_AUTH_PASSWORD)
+    user_tokens = user.generate_token()
     return {
-        'email': login_user.email ,
-        'username': login_user.username ,
+        'email': user.email ,
+        'username': user.username ,
         'access_token': str(user_tokens.get("access")) ,
         'refresh_token': str(user_tokens.get("refresh"))
     }
 
 # Todo make this more secure
-def authenticate_without_password(request, email):
+def authenticate_without_password_and_send_response(request, email):
     user = User.objects.filter(email = email).first()
 
     if user is not None:
