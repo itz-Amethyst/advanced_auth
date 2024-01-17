066e10766d23c5da2996ea839f3b1c94ae413fc4
itz-amethyst <pransermi@gmail.com>
2024-01-17 19:00:43 +0330

    feat: Completed resetpassword page

--- Frontend/src/pages/ResetPassword.svelte
+++ Frontend/src/pages/ResetPassword.svelte
@@ -1,42 +1,59 @@
 <script>
+  import {onMount} from "svelte"
   import { navigate } from "svelte-routing";
   import { capitalize } from "../utils/helper";
   import { showToast } from "../utils/toasthelper";
   import axiosInstanse from "../auth/axiosInstance";
-
+  import { checkIsFormFilled } from "../utils/helper";
 
   let formdata = {
     password: "",
     confirm_password: "",
   };
 
-  const isFormFilled = () => {
-    return Object.values(formdata).every((value) => value.trim() !== "");
-  };
-
+  let uid;
+  let token;
+  
   const handleOnChange = (e) => {
     formdata = { ...formdata, [e.target.name]: e.target.value };
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (isFormFilled()) {
-      try {
-        const res = await axiosInstanse.patch("api/auth/set-new-password/", {
-          password: formdata.password,
-          confirm_password: formdata.confirm_password,
-          uidb64: e.useParams.uid,
-          token: e.useParams.token,
-        });
-        const response = res.data;
-        if (res.status === 200) {
-          navigate("/login");
-          showToast("Success", response.message, "success");
-        }
-        console.log(response);
-      } catch (error) {
-        console.error("Error submitting form:", error);
+
+    if (!checkIsFormFilled(formdata)) {
+      showToast("Error", "You must filled all the blanks", "error");
+      return;
+    }
+
+    if (formdata.password !== formdata.confirm_password) {
+      showToast("Error", "Passwords do not match", "info");
+      return;
+    }
+
+    try {
+      const baseURI = e.target.baseURI
+      console.log(baseURI);
+
+      const segments = baseURI.split("/");
+      uid = segments[segments.length - 3];
+      token = segments[segments.length - 2];
+
+      const res = await axiosInstanse.patch("api/auth/set-new-password/", {
+        password: formdata.password,
+        confirm_password: formdata.confirm_password,
+        uidb64: uid,
+        token: token,
+      });
+      const response = res.data;
+      if (res.status === 200) {
+        navigate("/login");
+        showToast("Success", response.message, "success");
       }
+      console.log(response);
+    } catch (error) {
+      console.log(error.response.data.detail);
+      showToast("Error", error.response.data.detail, "error")
     }
   };
 </script>
@@ -52,6 +69,7 @@
             <input
               type="password"
               class="password-form"
+              minlength=6
               name={key}
               bind:value={formdata[key]}
               on:input={handleOnChange}
