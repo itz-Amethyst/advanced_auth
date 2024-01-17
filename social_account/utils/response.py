e446dd46bb06f1941ddf29b75bbff6fecd32fabb
itz-amethyst <pransermi@gmail.com>
2024-01-17 18:07:42 +0330

    feat: added authwithout password option

--- social_account/utils/response.py
+++ social_account/utils/response.py
@@ -1,5 +1,8 @@
 from django.conf import settings
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate , login
+from rest_framework.exceptions import AuthenticationFailed
+
+from account.models import User
 
 
 def send_response(email):
@@ -11,3 +14,20 @@ def send_response(email):
         'access_token': str(user_tokens.get("access")) ,
         'refresh_token': str(user_tokens.get("refresh"))
     }
+
+# Todo make this more secure
+def authenticate_without_password(request, email):
+    user = User.objects.filter(email = email).first()
+
+    if user is not None:
+        # Log in the user and generate datas in cookies
+        login(request , user)
+        user_tokens = user.generate_token()
+        return {
+            'email': user.email,
+            'username': user.username,
+            'access_token':  str(user_tokens.get("access")),
+            'refresh_token': str(user_tokens.get("refresh"))
+        }
+    else:
+        raise AuthenticationFailed(detail = "Authentication failed. User credentials are incorrect.")
